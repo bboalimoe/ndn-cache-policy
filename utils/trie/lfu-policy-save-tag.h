dedcf9f98cfd853a0e1125481c1aafb9b2739cbb
bboalimoe <danielzhang-1@163.com>
2014-11-27 17:20:36 +0800

    test

--- utils/trie/lfu-policy-save-tag.h
+++ utils/trie/lfu-policy-save-tag.h
@@ -1,6 +1,6 @@
-/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
 /*
- * Copyright (c) 2011 University of California, Los Angeles
+ * Copyright (c) 2014 BUPT
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Alexander Afanasyev <alexander.afanasyev@ucla.edu>
+ * Authors:    hengyang zhang <bboalimoe@gmail.com>
  */
 
 #ifndef LFU_POLICY_SAVE_TAG_H_
@@ -331,7 +331,7 @@ struct lfu_policy_save_tag_traits
 							tag)) {
 
 						if (tag.Get() == tag_value) {
-
+							std::cout << "cs的容量" << policy_container::size() << std::endl;
 							trieIt = &(*iter);
 							break;
 						}
